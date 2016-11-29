328edb0d8d7076b11c5b0c5811923f96c617bded
JackDrogon <jack.xsuperman@gmail.com>
2016-11-29 14:22:20 +0800

    Update ss.cc

--- Linux/ss.cc
+++ Linux/ss.cc
@@ -5,6 +5,8 @@
 using namespace std;
 
 // TODO: Parse other column from "/proc/net/tcp"
+// sl  local_address rem_address   st tx_queue rx_queue tr tm->when retrnsmt   uid  timeout inode
+// 0: 00000000:1F40 00000000:0000 0A 00000000:00000000 00:00000000 00000000     0        0 1394034504 1 ffff880436fcd7c0 99 0 0 10 -1
 
 static const string PROC_TCP_FILE = "/proc/net/tcp";
 
@@ -18,6 +20,7 @@ static string GetIpAddress(char *str)
 	oss << d << "." << c << "." << b << "." << a << ":" << e ;
 	return oss.str();
 }
+
 // Get tcp connection state
 static string GetConnectionState(char *str)
 {
@@ -41,25 +44,20 @@ static string GetConnectionState(char *str)
 
 int main()
 {
-	// read from file /proc/net/tcp
 	ifstream proc_tcp_file(PROC_TCP_FILE.c_str()); // FIXME: gcc-5 can't use ifstream proc_tcp_file(PROC_TCP_FILE);
 	if (!proc_tcp_file) {
 		cerr << "open ""<< error!" << endl;
 		exit(1);
 	}
-	// sl  local_address rem_address   st tx_queue rx_queue tr tm->when retrnsmt   uid  timeout inode
-	// 0: 00000000:1F40 00000000:0000 0A 00000000:00000000 00:00000000 00000000     0        0 1394034504 1 ffff880436fcd7c0 99 0 0 10 -1
 	string line;
-	char s_local_addr[20], s_remote_addr[20], s_state[20];
+	char local_addr[20], remote_addr[20], state[20];
 	getline(proc_tcp_file, line); // title: every column's name
 	while (getline(proc_tcp_file, line)) {
-		sscanf(line.c_str(), "%*s%s%s%s", s_local_addr, s_remote_addr,
-		       s_state);
-		string ip_local = GetIpAddress(s_local_addr);
-		string ip_remote = GetIpAddress(s_remote_addr);
-		string conn_state = GetConnectionState(s_state);
-		cout << ip_local << "\t" << ip_remote << "\t" << conn_state
-		     << endl;
+		sscanf(line.c_str(), "%*s%s%s%s", local_addr, remote_addr, state);
+		string ip_local = GetIpAddress(local_addr);
+		string ip_remote = GetIpAddress(remote_addr);
+		string conn_state = GetConnectionState(state);
+		cout << ip_local << "\t" << ip_remote << "\t" << conn_state << endl;
 	}
 	return 0;
 }

de65e9cf0c848c1e774f90a1e523596db167d0cd
Jack Drogon <jack.xsuperman@gmail.com>
2021-06-07 16:41:06 +0800

    Update list-cscache by pstore

--- Common/list-cscache.rb
+++ Common/list-cscache.rb
@@ -1,12 +1,14 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
+require 'pstore'
+
 CACHE_FILE = "#{ENV['HOME']}/.cs_cache"
 
-File.open(CACHE_FILE) do |file|
-  @cache = Marshal.load(file)
-end
-
-@cache.each do |key, value|
-  printf("%-13s =>   %s\n", key, value)
+cache = PStore.new(CACHE_FILE)
+cache.transaction(true) do
+  cache.roots.each do |key|
+    value = cache[key]
+    printf("%<key>-13s =>   %<value>s\n", key: key, value: value)
+  end
 end
